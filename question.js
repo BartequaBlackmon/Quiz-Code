0329cfdcf0d4281d6c71360fb39af300ddee8550
bartequa <bartequa@gmail.com>
2021-12-18 14:36:44 -0500

    add questions

---
+++ question.js
@@ -0,0 +1,48 @@
+// This is the question functions that contain questions and answers. They are in multidimensional array with inner arrayelements
+var questions = [
+    {
+        question: "Who is the president of the United States of America?",
+        answers: [
+            { text: "Joe Biden", correct: true },
+            { text: "Donald J. Trump", correct: false },
+            { text: "Bill Clinton", correct: false },
+            { text: "Barrack Obama", correct: false }
+        ]
+    },
+    {
+        question: "What is the capital city of North Carolina?",
+        answers: [
+            { text: "Richmond", correct: false },
+            { text: "Charlotte", correct: false },
+            { text: "Raleigh", correct: true },
+            { text: "Atlanta", correct: false }
+        ]
+    },
+    {
+        question: "In which city was the declaration of Independence signed?",
+        answers: [
+            { test: "Washington DC", correct: false },
+            { text: "New York", correct: false },
+            { text: "Philadelphia", correct: true },
+            { text: "Boston", correct: false }
+        ]
+    },
+    {
+        question: "How many stripes are on the United States flag?",
+        answers: [
+            { text: "15", correct: false },
+            { text: "50", correct: false },
+            { text: "48", correct: false },
+            { texr: "13", correct: true }
+        ]
+    },
+    {
+        question: "In what year did the United States of America gained her independence?",
+        answers: [
+            { text: "1900", correct: false },
+            { text: "1776", correct: true },
+            { text: "1492", correct: false },
+            { text: "1876", correct: false }
+        ]
+    },
+];
